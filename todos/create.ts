4aad6e7dff9ee1a3d3f6a578fd09f52283e8acd9
Júnior Conqusta <juniorconqusta@MacBook-Pro-de-Fabricio.local>
2021-01-26 09:59:38 -0300

    Initial commit

---
+++ todos/create.ts
@@ -0,0 +1,46 @@
+'use strict'
+
+import * as uuid from 'uuid'
+import { DynamoDB } from 'aws-sdk';
+
+const dynamoDb = new DynamoDB.DocumentClient();
+
+const create = (event: { body: string }, _: any, callback: (param: any, response?: {}) => void) => {
+    const timestamp = new Date().getTime()
+    const data = JSON.parse(event.body)
+    if (typeof data.text !== 'string') {
+        console.error('Validation Failed');
+        callback(new Error('Couldn\'t create the todo item.'));
+        return;
+    }
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Item: {
+            id: uuid.v1(),
+            text: data.text,
+            checked: false,
+            createdAt: timestamp,
+            updatedAt: timestamp
+        }
+    }
+
+    // write the todo to the database
+    //@ts-ignore
+    dynamoDb.put(params, (error) => {
+        // handle potential errors
+        if (error) {
+            console.error(error)
+            callback(new Error('Couldn\'t create the todo item.'))
+            return
+        }
+
+        // create a response
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(params.Item)
+        }
+        callback(null, response)
+    })
+};
+
+module.exports = { create };
